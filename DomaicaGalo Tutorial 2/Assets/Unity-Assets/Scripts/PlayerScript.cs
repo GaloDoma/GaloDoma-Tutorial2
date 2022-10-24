3f36543649f344e7a50c4b64b16825d5be1a66a2
GaloDoma <galodomaica@gmail.com>
2022-10-24 15:17:11 -0400

    Challenge changes

--- DomaicaGalo Tutorial 2/Assets/Unity-Assets/Scripts/PlayerScript.cs
+++ DomaicaGalo Tutorial 2/Assets/Unity-Assets/Scripts/PlayerScript.cs
@@ -13,11 +13,14 @@ public class PlayerScript : MonoBehaviour
 
     private int scoreValue = 0;
 
+    public GameObject winText;
+
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        winText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -35,6 +38,11 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+        }
+
+        if (scoreValue >= 7)
+        {
+            winText.SetActive(true);
         }
 
     }
